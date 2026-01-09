bc50e3b5e592884c000257e52a0d15968a3f6646
fusuke komazawa <124cr0009@tokyo.amg.ac.jp>
2026-01-09 17:31:04 +0900

    フレーム仮作成

--- Chain_Project/source/Scene_UI_Button.cpp
+++ Chain_Project/source/Scene_UI_Button.cpp
@@ -10,10 +10,12 @@
 Scene_UI_Button::Scene_UI_Button(const std::string& cName, const int iLayer) : Scene_Base(cName, iLayer, false, false)
 {
 	/* 初期化 */
-	this->iFontHandle	= 0;		// フォントハンドル
-	this->ButtonText	= "";		// ボタンテキスト
-	this->CenterPos		= { 0, 0 };	// 位置座標
-	this->bMouseOverFlg	= false;	// カーソルが重なっている状態であるかのフラグ
+	this->iFontHandle		= 0;		// フォントハンドル
+	this->ButtonText		= "";		// ボタンテキスト
+	this->CenterPos			= { 0, 0 };	// 位置座標
+	this->bMouseOverFlg		= false;	// カーソルが重なっている状態であるかのフラグ
+	this->Anim_ImageNo		= 0;		// アニメーションの画像番号
+	this->Anim_ChangeDelay	= 0;		// アニメーションの変更までの待機時間
 
 	/* 画像読み込み */
 	// 画像管理データリスト取得
@@ -33,6 +35,19 @@ Scene_UI_Button::Scene_UI_Button(const std::string& cName, const int iLayer) : S
 	this->Image_Frame_Inside[0] = pDataList_Image->iGetImageHandle(ImageFilePath);;
 	ImageFilePath = "UI/Button/Button_Frame_Inside_Over";
 	this->Image_Frame_Inside[1] = pDataList_Image->iGetImageHandle(ImageFilePath);;
+
+	// 強調表示アニメーション(角)
+	for(int i = 0; i < EMPHASIS_ANIMATION_MAX; i++)
+	{
+		ImageFilePath = "UI/Button/Emphasis_Anim/Button_Emphasis_Corner_" + std::to_string(i + 1);
+		this->Image_Frame_Emphasis[0][i] = pDataList_Image->iGetImageHandle(ImageFilePath);
+	}
+	// 強調表示アニメーション(線)
+	for(int i = 0; i < EMPHASIS_ANIMATION_MAX; i++)
+	{
+		ImageFilePath = "UI/Button/Emphasis_Anim/Button_Emphasis_Line_" + std::to_string(i + 1);
+		this->Image_Frame_Emphasis[1][i] = pDataList_Image->iGetImageHandle(ImageFilePath);
+	}
 }
 
 // 更新
@@ -56,6 +71,37 @@ void Scene_UI_Button::Update()
 	if (PUBLIC_PROCESS::bPositionIn2DRangeCheck(MousePosition, Range))
 	{
 		// マウスカーソルがボタンの範囲内にある場合
+		/* 初めて重なった場合であるか確認 */
+		if (this->bMouseOverFlg == false)
+		{
+			// 初めて重なった場合
+			/* アニメーションをリセット */
+			this->Anim_ImageNo		= 0;
+			this->Anim_ChangeDelay	= EMPHASIS_ANIMATION_SPEED;
+		}
+		else
+		{
+			// 初めてではない場合
+			/* アニメーションを更新 */
+			if (this->Anim_ChangeDelay <= 0)
+			{
+				this->Anim_ChangeDelay = EMPHASIS_ANIMATION_SPEED;
+
+				if (this->Anim_ImageNo >= EMPHASIS_ANIMATION_MAX - 1)
+				{
+					this->Anim_ImageNo = 0;
+				}
+				else
+				{
+					this->Anim_ImageNo++;
+				}
+			}
+			else
+			{
+				this->Anim_ChangeDelay--;
+			}
+
+		}
 		this->bMouseOverFlg = true;
 	}
 	else
@@ -148,6 +194,140 @@ void Scene_UI_Button::Draw()
 		this->CenterPos.iX - iSizeX / 2,	this->CenterPos.iY + iSizeY / 2,
 		*(this->Image_Frame_Inside[iImageIndex]), TRUE
 	);
+
+	/* 強調表示アニメーションを描写 */
+	if (this->bMouseOverFlg)
+	{
+		// 角(左上)
+		DrawModiGraph(
+			this->CenterPos.iX - iSizeX / 2 - EMPHASIS_ANIMATION_THICKNESS,	this->CenterPos.iY - iSizeY / 2 - EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX - iSizeX / 2,								this->CenterPos.iY - iSizeY / 2 - EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX - iSizeX / 2,								this->CenterPos.iY - iSizeY / 2,
+			this->CenterPos.iX - iSizeX / 2 - EMPHASIS_ANIMATION_THICKNESS,	this->CenterPos.iY - iSizeY / 2,
+			*(this->Image_Frame_Emphasis[0][this->Anim_ImageNo]), TRUE
+		);
+		// 角(右上)
+		DrawModiGraph(
+			this->CenterPos.iX + iSizeX / 2 + EMPHASIS_ANIMATION_THICKNESS, this->CenterPos.iY - iSizeY / 2 - EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX + iSizeX / 2 + EMPHASIS_ANIMATION_THICKNESS, this->CenterPos.iY - iSizeY / 2,
+			this->CenterPos.iX + iSizeX / 2,								this->CenterPos.iY - iSizeY / 2,
+			this->CenterPos.iX + iSizeX / 2,								this->CenterPos.iY - iSizeY / 2 - EMPHASIS_ANIMATION_THICKNESS,
+			*(this->Image_Frame_Emphasis[0][this->Anim_ImageNo]), TRUE
+		);
+		// 角(右下)
+		DrawModiGraph(
+			this->CenterPos.iX + iSizeX / 2 + EMPHASIS_ANIMATION_THICKNESS,	this->CenterPos.iY + iSizeY / 2 + EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX + iSizeX / 2,								this->CenterPos.iY + iSizeY / 2 + EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX + iSizeX / 2,								this->CenterPos.iY + iSizeY / 2,
+			this->CenterPos.iX + iSizeX / 2 + EMPHASIS_ANIMATION_THICKNESS, this->CenterPos.iY + iSizeY / 2,
+			*(this->Image_Frame_Emphasis[0][this->Anim_ImageNo]), TRUE
+		);
+		// 角(左下)
+		DrawModiGraph(
+			this->CenterPos.iX - iSizeX / 2 - EMPHASIS_ANIMATION_THICKNESS,	this->CenterPos.iY + iSizeY / 2 + EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX - iSizeX / 2 - EMPHASIS_ANIMATION_THICKNESS,	this->CenterPos.iY + iSizeY / 2,
+			this->CenterPos.iX - iSizeX / 2,								this->CenterPos.iY + iSizeY / 2,
+			this->CenterPos.iX - iSizeX / 2,								this->CenterPos.iY + iSizeY / 2 + EMPHASIS_ANIMATION_THICKNESS,
+			*(this->Image_Frame_Emphasis[0][this->Anim_ImageNo]), TRUE
+		);
+		// 線(上)
+		DrawModiGraph(
+			this->CenterPos.iX - iSizeX / 2, this->CenterPos.iY - iSizeY / 2 - EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX + iSizeX / 2, this->CenterPos.iY - iSizeY / 2 - EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX + iSizeX / 2, this->CenterPos.iY - iSizeY / 2,
+			this->CenterPos.iX - iSizeX / 2, this->CenterPos.iY - iSizeY / 2,
+			*(this->Image_Frame_Emphasis[1][this->Anim_ImageNo]), TRUE
+		);
+		// 線(右)
+		DrawModiGraph(
+			this->CenterPos.iX + iSizeX / 2 + EMPHASIS_ANIMATION_THICKNESS, this->CenterPos.iY + iSizeY / 2,
+			this->CenterPos.iX + iSizeX / 2 + EMPHASIS_ANIMATION_THICKNESS, this->CenterPos.iY - iSizeY / 2,
+			this->CenterPos.iX + iSizeX / 2,								this->CenterPos.iY - iSizeY / 2,
+			this->CenterPos.iX + iSizeX / 2,								this->CenterPos.iY + iSizeY / 2,
+			*(this->Image_Frame_Emphasis[1][this->Anim_ImageNo]), TRUE
+		);
+		// 線(下)
+		DrawModiGraph(
+			this->CenterPos.iX + iSizeX / 2, this->CenterPos.iY + iSizeY / 2 + EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX - iSizeX / 2, this->CenterPos.iY + iSizeY / 2 + EMPHASIS_ANIMATION_THICKNESS,
+			this->CenterPos.iX - iSizeX / 2, this->CenterPos.iY + iSizeY / 2,
+			this->CenterPos.iX + iSizeX / 2, this->CenterPos.iY + iSizeY / 2,
+			*(this->Image_Frame_Emphasis[1][this->Anim_ImageNo]), TRUE
+		);
+		// 線(左)
+		DrawModiGraph(
+			this->CenterPos.iX - iSizeX / 2 - EMPHASIS_ANIMATION_THICKNESS, this->CenterPos.iY - iSizeY / 2,
+			this->CenterPos.iX - iSizeX / 2 - EMPHASIS_ANIMATION_THICKNESS, this->CenterPos.iY + iSizeY / 2,
+			this->CenterPos.iX - iSizeX / 2,								this->CenterPos.iY + iSizeY / 2,
+			this->CenterPos.iX - iSizeX / 2,								this->CenterPos.iY - iSizeY / 2,
+			*(this->Image_Frame_Emphasis[1][this->Anim_ImageNo]), TRUE
+		);
+
+		//// 角(左上)
+		//DrawModiGraph(
+		//	this->CenterPos.iX - iSizeX / 2 - FRAME_THICKNESS,	this->CenterPos.iY - iSizeY / 2 - FRAME_THICKNESS,
+		//	this->CenterPos.iX - iSizeX / 2,					this->CenterPos.iY - iSizeY / 2 - FRAME_THICKNESS,
+		//	this->CenterPos.iX - iSizeX / 2,					this->CenterPos.iY - iSizeY / 2,
+		//	this->CenterPos.iX - iSizeX / 2 - FRAME_THICKNESS,	this->CenterPos.iY - iSizeY / 2,
+		//	*(this->Image_Frame_Emphasis[0][this->Anim_ImageNo]), TRUE
+		//);
+		//// 角(右上)
+		//DrawModiGraph(
+		//	this->CenterPos.iX + iSizeX / 2 + FRAME_THICKNESS,	this->CenterPos.iY - iSizeY / 2 - FRAME_THICKNESS,
+		//	this->CenterPos.iX + iSizeX / 2 + FRAME_THICKNESS,	this->CenterPos.iY - iSizeY / 2,
+		//	this->CenterPos.iX + iSizeX / 2,					this->CenterPos.iY - iSizeY / 2,
+		//	this->CenterPos.iX + iSizeX / 2,					this->CenterPos.iY - iSizeY / 2 - FRAME_THICKNESS,
+		//	*(this->Image_Frame_Emphasis[0][this->Anim_ImageNo]), TRUE
+		//);
+		//// 角(右下)
+		//DrawModiGraph(
+		//	this->CenterPos.iX + iSizeX / 2 + FRAME_THICKNESS,	this->CenterPos.iY + iSizeY / 2 + FRAME_THICKNESS,
+		//	this->CenterPos.iX + iSizeX / 2,					this->CenterPos.iY + iSizeY / 2 + FRAME_THICKNESS,
+		//	this->CenterPos.iX + iSizeX / 2,					this->CenterPos.iY + iSizeY / 2,
+		//	this->CenterPos.iX + iSizeX / 2 + FRAME_THICKNESS,	this->CenterPos.iY + iSizeY / 2,
+		//	*(this->Image_Frame_Emphasis[0][this->Anim_ImageNo]), TRUE
+		//);
+		//// 角(左下)
+		//DrawModiGraph(
+		//	this->CenterPos.iX - iSizeX / 2 - FRAME_THICKNESS,	this->CenterPos.iY + iSizeY / 2 + FRAME_THICKNESS,
+		//	this->CenterPos.iX - iSizeX / 2 - FRAME_THICKNESS,	this->CenterPos.iY + iSizeY / 2,
+		//	this->CenterPos.iX - iSizeX / 2,					this->CenterPos.iY + iSizeY / 2,
+		//	this->CenterPos.iX - iSizeX / 2,					this->CenterPos.iY + iSizeY / 2 + FRAME_THICKNESS,
+		//	*(this->Image_Frame_Emphasis[0][this->Anim_ImageNo]), TRUE
+		//);
+		//// 線(上)
+		//DrawModiGraph(
+		//	this->CenterPos.iX - iSizeX / 2, this->CenterPos.iY - iSizeY / 2 - FRAME_THICKNESS,
+		//	this->CenterPos.iX + iSizeX / 2, this->CenterPos.iY - iSizeY / 2 - FRAME_THICKNESS,
+		//	this->CenterPos.iX + iSizeX / 2, this->CenterPos.iY - iSizeY / 2,
+		//	this->CenterPos.iX - iSizeX / 2, this->CenterPos.iY - iSizeY / 2,
+		//	*(this->Image_Frame_Emphasis[1][this->Anim_ImageNo]), TRUE
+		//);
+		//// 線(右)
+		//DrawModiGraph(
+		//	this->CenterPos.iX + iSizeX / 2 + FRAME_THICKNESS,	this->CenterPos.iY + iSizeY / 2,
+		//	this->CenterPos.iX + iSizeX / 2 + FRAME_THICKNESS,	this->CenterPos.iY - iSizeY / 2,
+		//	this->CenterPos.iX + iSizeX / 2,					this->CenterPos.iY - iSizeY / 2,
+		//	this->CenterPos.iX + iSizeX / 2,					this->CenterPos.iY + iSizeY / 2,
+		//	*(this->Image_Frame_Emphasis[1][this->Anim_ImageNo]), TRUE
+		//);
+		//// 線(下)
+		//DrawModiGraph(
+		//	this->CenterPos.iX + iSizeX / 2, this->CenterPos.iY + iSizeY / 2 + FRAME_THICKNESS,
+		//	this->CenterPos.iX - iSizeX / 2, this->CenterPos.iY + iSizeY / 2 + FRAME_THICKNESS,
+		//	this->CenterPos.iX - iSizeX / 2, this->CenterPos.iY + iSizeY / 2,
+		//	this->CenterPos.iX + iSizeX / 2, this->CenterPos.iY + iSizeY / 2,
+		//	*(this->Image_Frame_Emphasis[1][this->Anim_ImageNo]), TRUE
+		//);
+		//// 線(左)
+		//DrawModiGraph(
+		//	this->CenterPos.iX - iSizeX / 2 - FRAME_THICKNESS,	this->CenterPos.iY - iSizeY / 2,
+		//	this->CenterPos.iX - iSizeX / 2 - FRAME_THICKNESS,	this->CenterPos.iY + iSizeY / 2,
+		//	this->CenterPos.iX - iSizeX / 2,					this->CenterPos.iY + iSizeY / 2,
+		//	this->CenterPos.iX - iSizeX / 2,					this->CenterPos.iY - iSizeY / 2,
+		//	*(this->Image_Frame_Emphasis[1][this->Anim_ImageNo]), TRUE
+		//);
+	}
 
 	/* ボタンテキスト描写 */
 	DrawStringToHandle(
--- Chain_Project/source/Scene_UI_Button.h
+++ Chain_Project/source/Scene_UI_Button.h
@@ -26,16 +26,22 @@ class Scene_UI_Button : public Scene_Base
 		bool GetMouseOverFlg() const { return this->bMouseOverFlg; }		// カーソルが重なっている状態であるかのフラグを取得
 
 		/* 定数 */
-		static const int FRAME_THICKNESS = 16;	// 枠の太さ
+		static const int FRAME_THICKNESS				= 16;	// 枠の太さ
+		static const int EMPHASIS_ANIMATION_THICKNESS	= 32;	// 強調表示アニメーションの太さ
+		static const int EMPHASIS_ANIMATION_MAX			= 5;	// 強調表示アニメーションの最大数
+		static const int EMPHASIS_ANIMATION_SPEED		= 10;	// 強調表示アニメーションの速度(変更までのフレーム数)
 
 	private:
 		/* 変数 */
-		int					iFontHandle;	// フォントハンドル
-		std::string			ButtonText;		// ボタンテキスト
-		Struct_2D::POSITION	CenterPos;		// 位置座標
-		bool				bMouseOverFlg;	// カーソルが重なっている状態であるかのフラグ
+		int					iFontHandle;		// フォントハンドル
+		std::string			ButtonText;			// ボタンテキスト
+		Struct_2D::POSITION	CenterPos;			// 位置座標
+		bool				bMouseOverFlg;		// カーソルが重なっている状態であるかのフラグ
+		int					Anim_ImageNo;		// アニメーションの画像番号
+		int					Anim_ChangeDelay;	// アニメーションの変更までの待機時間
 		// 画像
-		std::shared_ptr<int>	Image_Frame_Corner[2];	// 角[0:通常時, 1:カーソル接触時]
-		std::shared_ptr<int>	Image_Frame_Line[2];	// 線[0:通常時, 1:カーソル接触時]
-		std::shared_ptr<int>	Image_Frame_Inside[2];	// 内側[0:通常時, 1:カーソル接触時]
+		std::shared_ptr<int>	Image_Frame_Corner[2];								// 角[0:通常時, 1:カーソル接触時]
+		std::shared_ptr<int>	Image_Frame_Line[2];								// 線[0:通常時, 1:カーソル接触時]
+		std::shared_ptr<int>	Image_Frame_Inside[2];								// 内側[0:通常時, 1:カーソル接触時]
+		std::shared_ptr<int>	Image_Frame_Emphasis[2][EMPHASIS_ANIMATION_MAX];	// 強調表示アニメーション[0:角, 1:線][アニメーション番号]
 };
